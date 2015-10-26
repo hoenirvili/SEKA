991994dd22e38a19e513b81f544f0527d30fcf0e
hoenirvili <hoenirvili@gmail.com>
2015-10-26 20:52:44 +0200

    animation menu close clunky

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+/**
+ * We pass our jquery object
+ * into self invoking lambda function
+ * We've making this lambda function a
+ * self contain module.
+ */
+(function($) {
+    /**
+     * function that check's if
+     * we have the class it will remove
+     * or if we don't have the class it will
+     * be added to the our button.
+     */
+    function openMenu() {
+        var $this = $(this),
+            menu = $('.menu');
+
+        menu.toggleClass('menu-open');
+        $this.toggleClass('hide-menu-button');
+    }
+
+    function closeMenu() {
+        //var $this = $(this);
+        var menu = $('.menu');
+        var button = $('.menu-button');
+        menu.toggleClass('menu-open');
+        //make button reappear
+        button.toggleClass('hide-menu-button');
+
+    }
+
+    /**
+     * If the document is loaded execute this function
+     */
+    $(document).ready(function() {
+        $('.menu-button').on('click',openMenu);
+        $('.menu-close').on('click', closeMenu);
+    });
+
+})(jQuery);
