23645247bdc0e27b5842e449f0688e8782d2702c
hoenirvili <hoenirvili@gmail.com>
2015-10-27 12:30:20 +0200

    fixed ident issues

--- src/js/index.js
+++ src/js/index.js
@@ -5,41 +5,41 @@
  * self contain module.
  */
 (function($) {
-    /**
-     * Just internet explorer compatibility issues
-     * in case of anything else.
-     */
-    "use strict"
-    /**
-     * function that check's if
-     * we have the class it will remove
-     * or if we don't have the class it will
-     * be added to the our button.
-     */
-    function openMenu() {
-        var $this = $(this),
-            menu = $('.menu');
+	/**
+ 	 * Just internet explorer compatibility issues
+	 * in case of anything else.
+	 */
+	 "use strict"
+	 /**
+	 * function that check's if
+	 * we have the class it will remove
+	 * or if we don't have the class it will
+	 * be added to the our button.
+	 */
+	 function openMenu() {
+		var $this = $(this),
+			menu = $('.menu');
 
-        menu.toggleClass('menu-open');
-        $this.toggleClass('hide-menu-button');
-    }
+		menu.toggleClass('menu-open');
+		$this.toggleClass('hide-menu-button');
+	}
 
-    function closeMenu() {
-        //var $this = $(this);
-        var menu = $('.menu');
-        var button = $('.menu-button');
-        menu.toggleClass('menu-open');
-        //make button reappear
-        button.toggleClass('hide-menu-button');
+	function closeMenu() {
+		//var $this = $(this);
+		var menu = $('.menu');
+		var button = $('.menu-button');
+		menu.toggleClass('menu-open');
+		//make button reappear
+		button.toggleClass('hide-menu-button');
 
-    }
+	}
 
-    /**
-     * If the document is loaded execute this function
-     */
-    $(document).ready(function() {
-        $('.menu-button').on('click',openMenu);
-        $('.menu-close').on('click', closeMenu);
-    });
+	/**
+	 * If the document is loaded execute this function
+	 */
+	 $(document).ready(function() {
+		 $('.menu-button').on('click',openMenu);
+		 $('.menu-close').on('click', closeMenu);
+	 });
 
 })(jQuery);
