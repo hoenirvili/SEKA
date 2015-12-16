1058951090e78fd00c1728a7718148e43b13b4f1
hoenirvili <hoenirvili@gmail.com>
2015-12-16 15:28:34 +0200

    add query string validation pop-up

--- src/js/search-action.js
+++ src/js/search-action.js
@@ -33,15 +33,12 @@
 		// intersting enough when return false is triggered
 		// it also stops callback execution.
 	}
-	
-	function searchAction() {
-		// it's a good practice to create and cache all
-		// local var in the top of the function/clouj etc..
+	function filterSearchAction(queryString, filters) {
 		var i;
+		if(filters) {
+			for (i=0; i<filters; i++) {
+				switch(options[i]) {
 
-		for (i=0; i<options.length; i++) 
-		{
-			switch(options[i]) {
 				case "DuckDuckGo":
 					break;
 
@@ -50,15 +47,40 @@
 
 				case "Google":
 					break;
-
 				case "Facebook":
-					console.log("test");
+				// TODO: make facebook request
+					var token="1523007798014505%7CNTm64aS3PIH_-Mfm8NAyQ1NGsp0"
+					$.getJSON("https://graph.facebook.com/10152716010956729/photos?fields=source,picture,link,name&"+"&access_token="+token, function(data){
+						console.log(data);
+					});
 					break;
-
 				case "Bing":
 					break;	
-			}
+				}
+			}//for
+		} else {
+			//TODO Make all requests
+			console.log("make all request test");
 		}
+	}	
+	function searchAction() {
+		// cache all jquery obj $(this)
+		// for performance reasons
+		var $this = $(this);
+		// it's a good practice to create and cache all
+		// local var in the top of the function/clouj etc..
+		var i;
+		var queryString = $('#search-query').val();
+		// test if query string is empty
+		if (queryString !=="") {
+			var filters = options.length;
+			filterSearchAction(queryString, filters)
+		// if query string is empty show error
+		} else {
+			// init data toggle and show immediately
+			$('[data-toggle="tooltip"]').tooltip().tooltip('show');
+		}
+
 	}
 
 
