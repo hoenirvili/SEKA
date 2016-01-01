70ee914bb86e21d5ff7d3d552577d2e1f1bb8a85
hoenirvili <hoenirvili@gmail.com>
2016-01-01 22:42:34 +0200

    reorder some methods

--- src/js/facebook.js
+++ src/js/facebook.js
@@ -1,12 +1,16 @@
 define("facebook", ["template", "apicfg", "jquery"], function(template, apicfg, $) {
-
+	// main request link
 	var req;
+	// hold the first json
 	var temp;
+	// hold every id
 	var tempi;
 	
-	var appendResults = function(results) {
-		tempi = results.id;
-		$('.facebook-results > ul').append(template.results(temp.data[tempi].id, temp.data[tempi].name, results.about)).appendTo('.search-result-wrapper');
+
+	var request = function(queryString) {
+		//build the request
+		req = apicfg.facebook.url + queryString + apicfg.facebook.type + apicfg.facebook.limit + apicfg.facebook.token;
+		$.getJSON(req, getPageList);
 	};
 
 	var getPageList = function(result) {
@@ -14,15 +18,17 @@ define("facebook", ["template", "apicfg", "jquery"], function(template, apicfg, 
 		temp = result;
 		for (i=0; i<result.data.length; i++) {
 			temp.data[result.data[i].id] = result.data[i];
-			$.getJSON('https://graph.facebook.com/' + result.data[i].id + '/?fields=about' + apicfg.facebook.token, appendResults);//function
+			$.getJSON('https://graph.facebook.com/' + result.data[i].id + '/?fields=about' + apicfg.facebook.token, appendResults);
 		}
 	};
 
-	var request = function(queryString) {
-		//build the request
-		req = apicfg.facebook.url + queryString + apicfg.facebook.type + apicfg.facebook.limit + apicfg.facebook.token;
-		$.getJSON(req, getPageList);
+	var appendResults = function(results) {
+		tempi = results.id;
+		$('.facebook-results > ul').append(
+			template.results(temp.data[tempi].id, temp.data[tempi].name, results.about)
+		).appendTo('.search-result-wrapper');
 	};
+
 
 	return {
 		request: request
